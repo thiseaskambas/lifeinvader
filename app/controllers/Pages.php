ecba7d45ab11df7aa329fc9ba565d05ed7eedf7b
thiseaskambas <tkambas@yahoo.com>
2023-03-16 19:19:31 -0400

    base setup

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,14 +10,20 @@ class Pages extends Controller
    {
 
       $data = [
-         'title' => 'Welcome',
+         'title' => 'LifeInvader',
+         'subtitle' => 'Join our community of millions of users who love sharing every little detail of their lives.',
+         'description' => ' * Just kidding, this is a simple social network built on a custom MVC framework with PHP.'
       ];
 
       $this->view("pages/index", $data);
    }
    public function about()
    {
-      $data = ['title' => 'About'];
+      $data = [
+         'title' => 'About',
+         'subtitle' => 'Sign up now and let the invasion begin!',
+         'description' => "Welcome to Life Invader, the social network that's so addictive, you'll forget about the rest of your life! Join our community of millions of users who love sharing every little detail of their lives, from their favorite pizza toppings to their morning bathroom routine. Plus, with our state-of-the-art stalking technology, you can keep tabs on all your friends (and enemies) 24/7, so you never miss a beat. So why waste your time with real-life social interactions when you can join the ultimate online time-suck?"
+      ];
       $this->view("pages/about", $data);
    }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,10 @@
 <?php require APPROOT . '/views/inc/header.php' ?>
-<h1><?php echo $data['title']; ?></h1>
+<div class="p-5 mb-4 bg-light rounded-3 text-center">
+   <div class="container-fluid py-5">
+      <h1 class="display-3 fw-bold"><?php echo $data['title']; ?></h1>
+      <p class="col-md-8 fs-4 mx-auto"><?php echo $data['subtitle']; ?></p>
+      <hr class="my-4">
+      <p class="col-md-8 fs-5 mx-auto fst-italic"><?php echo $data['description']; ?></p>
+   </div>
+</div>
 <?php require APPROOT . '/views/inc/footer.php' ?>
