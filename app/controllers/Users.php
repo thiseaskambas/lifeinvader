e211b6bf4c1ea2288c59a19c888683caf7de0051
thiseaskambas <tkambas@yahoo.com>
2023-03-17 15:15:58 -0400

    fix login form, getting data from signup and login forms

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -23,6 +23,29 @@ class Users extends Controller
             'password_err' => '',
             'password_conf_err' => '',
          ];
+
+         if (empty($data['name'])) {
+            $data['name_err'] = 'Please fill in your name';
+         }
+         if (empty($data['email'])) {
+            $data['email_err'] = 'Please fill in your email';
+         }
+         if (empty($data['password'])) {
+            $data['password_err'] = 'Please fill in a password';
+         } else if (strlen($data['password']) < 6) {
+            $data['password_err'] = 'Password must have at least 6 chatacters';
+         }
+         if (empty($data['password_conf'])) {
+            $data['password_conf_err'] = 'Please confirm your password';
+         } else if ($data['password_conf'] !== $data['password']) {
+            $data['password_conf_err'] = 'Passwords do not match';
+         }
+
+         if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['password_conf_err'])) {
+            die('✅');
+         } else {
+            $this->view('users/signup', $data);
+         }
       } else {
          $data = [
             'name' => '',
@@ -42,6 +65,24 @@ class Users extends Controller
    {
       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
          //process form
+         //Sanitize data
+         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+         $data = [
+            'email' => trim($_POST['email']),
+            'password' => trim($_POST['password']),
+            'email_err' => '',
+            'password_err' => '',
+         ];
+
+         if (empty($data['email'])) {
+            $data['email_err'] = 'Please fill in your email';
+         }
+         if (empty($data['password'])) {
+            $data['password_err'] = 'Please fill in a password';
+         }
+
+         $this->view('users/login', $data);
       } else {
          $data = [
             'email' => '',
--- app/views/users/signup.php
+++ app/views/users/signup.php
@@ -17,7 +17,7 @@
             </div>
             <div class="mb-3">
                <label for="password">Password: <sup>*</sup></label>
-               <input type="password" name="password_err" class="form-control form-control-lg <?php echo (empty($data['password_err'])) ? '' : 'is-invalid' ?> " value="<?php echo $data['password'] ?>">
+               <input type="password" name="password" class="form-control form-control-lg <?php echo (empty($data['password_err'])) ? '' : 'is-invalid' ?> " value="<?php echo $data['password'] ?>">
                <span class="invalid-feedback"><?php echo $data['password_err'] ?></span>
             </div>
             <div class="mb-3">
