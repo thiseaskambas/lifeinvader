e7b217e6e3368daa4554157361d8bdd7dc891ea7
thiseaskambas <tkambas@yahoo.com>
2023-03-17 11:18:49 -0400

    clean gitignore

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+// App Root -> for including stuff from within ./app
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL root -> for including stuff from within ./public - NO slash at the end 
+define('URLROOT', 'http://localhost/lifeinvader');
+
+//Site name
+define('SITENAME', 'LifeInvader');
+
+
+//DB params
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'kalamari');
+define('DB_NAME', 'life_invader');
